770e2ec9ddcd9ec5035cf10ca0ec9086b994ce83
NamMoc <shelter2055@gmail.com>
2026-04-03 02:23:02 +0700

    Delete langge/src directory

--- langge/src/config.js
+++
@@ -1,7 +0,0 @@
-var BASE_URL = "https://api.langge.cf";
-try {
-    if (CONFIG_URL) {
-        BASE_URL = CONFIG_URL;
-    }
-} catch (error) {
-}
--- langge/src/home.js
+++
@@ -1,9 +0,0 @@
-// home.js - Chon binh dai (Platform) -> genre.js hien thi the loai
-load("config.js");
-
-function execute() {
-    return Response.success([
-        { title: "🍅 Cà Chua (番茄)", input: "fanqie", script: "genre.js" },
-        { title: "📱 QQ Đọc (QQ阅读)", input: "qqread", script: "genre.js" }
-    ]);
-}
